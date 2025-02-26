2ac5e89da0f699fed177f231c0d54b28f6874b80
PedroLucasAlmah <pedro.lucas@almahcomunicacao.com.br>
2025-02-26 17:41:34 -0300

    feat: jwt authentication principles

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
-import type { FastifyInstance } from "fastify";
+import { FastifyInstance } from "fastify";
+import { authenticate } from "@/http/controllers/authenticate.controller";
 import { register } from "./controllers/register.controller";
-import { authenticate } from "./controllers/authenticate.controller";
+import { profile } from "@/http/controllers/profile.controller";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  /** Authenticated */
+  app.get("/me", profile);
 }
