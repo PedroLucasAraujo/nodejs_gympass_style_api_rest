895326d620dac0138387199199b6f9e8f18b343d
PedroLucasAlmah <pedro.lucas@almahcomunicacao.com.br>
2025-02-26 17:58:31 -0300

    feat: implement jwt on fastify

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -1,7 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 
@@ -19,17 +17,28 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
+    });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
-      return reply.status(409).send({ message: err.message });
+      return reply.status(400).send({ message: err.message });
     }
 
     throw err;
   }
-
-  return reply.status(200).send();
 }
