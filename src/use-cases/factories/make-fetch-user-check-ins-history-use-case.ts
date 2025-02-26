c2d679d8cd708acb975f1519e3381099cce7a356
PedroLucasAlmah <pedro.lucas@almahcomunicacao.com.br>
2025-02-26 19:51:58 -0300

    feat: create gyms controllers

--- src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -1,9 +1,9 @@
-import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository";
-import { FetchUserCheckInsHistoryUseCase } from "../fetch-user-check-ins-history";
+import { FetchNearbyGymsUseCase } from "../fetch-nearby-gyms";
+import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
 
-export function makeFetchUserCheckInsHistoryUseCase() {
-  const checkInsRepository = new PrismaCheckInsRepository();
-  const useCase = new FetchUserCheckInsHistoryUseCase(checkInsRepository);
+export function makeFetchNearbyGymsUseCase() {
+  const gymsRepository = new PrismaGymsRepository();
+  const useCase = new FetchNearbyGymsUseCase(gymsRepository);
 
   return useCase;
 }
