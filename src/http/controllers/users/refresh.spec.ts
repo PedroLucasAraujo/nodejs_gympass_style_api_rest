b094b255535e880c26306e6e7ee89e0e6d583fbf
PedroLucasAlmah <pedro.lucas@almahcomunicacao.com.br>
2025-02-27 18:38:03 -0300

    feat: implements role based authorization

--- src/http/controllers/users/refresh.spec.ts
+++ src/http/controllers/users/refresh.spec.ts
@@ -1,41 +1,38 @@
-import request from "supertest";
-import { app } from "@/app";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { FastifyReply, FastifyRequest } from "fastify";
 
-describe("Refresh Token (e2e)", () => {
-  beforeAll(async () => {
-    await app.ready();
-  });
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true });
 
-  afterAll(async () => {
-    await app.close();
-  });
+  const { role } = request.user;
 
-  it("should be able to refresh a token", async () => {
-    await request(app.server).post("/users").send({
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "123456",
+  const token = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    }
+  );
+
+  const refreshToken = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: "7d",
+      },
+    }
+  );
+
+  return reply
+    .setCookie("refreshToken", refreshToken, {
+      path: "/",
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
     });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const cookies = authResponse.get("Set-Cookie") ?? [];
-
-    const response = await request(app.server)
-      .patch("/token/refresh")
-      .set("Cookie", cookies)
-      .send();
-
-    expect(response.status).toEqual(200);
-    expect(response.body).toEqual({
-      token: expect.any(String),
-    });
-    expect(response.get("Set-Cookie")).toEqual([
-      expect.stringContaining("refreshToken="),
-    ]);
-  });
-});
+}
