8b22f85450775cd4fdc12537fedf76cf11b0319e
PedroLucasAlmah <pedro.lucas@almahcomunicacao.com.br>
2025-02-27 18:22:48 -0300

    feat: implementation of refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from "@/http/controllers/users/authenticate.controller";
 import { register } from "./register.controller";
 import { profile } from "@/http/controllers/users/profile.controller";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   /** Authenticated */
   app.get(
